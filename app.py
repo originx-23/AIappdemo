a3d78f3ca5378805ebc01b16726bfc32e6962991
Aric Smith <42173584+originx-23@users.noreply.github.com>
2024-05-17 13:51:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,17 @@
 import gradio as gr
-import torch
-from torchvision import models, transforms
+from lmdeploy import pipeline, TurbomindEngineConfig
 
-# 加载本地预训练模型
-model = models.resnet18()
-model.load_state_dict(torch.load('./resnet18-5c106cde.pth'))
-model.eval()
 
-# 从本地文件加载标签，假设你已经手动下载并保存为 "imagenet_classes.txt"
-with open("imagenet_classes.txt", "r") as f:
-    labels = f.read().split("\n")
+backend_config = TurbomindEngineConfig(session_len=8192) # 图片分辨率较高时请调高session_len
+# pipe = pipeline('liuhaotian/llava-v1.6-vicuna-7b', backend_config=backend_config) 非开发机运行此命令
+pipe = pipeline('/share/new_models/liuhaotian/llava-v1.6-vicuna-7b', backend_config=backend_config)
 
-def predict(inp):
-    inp = transforms.ToTensor()(inp).unsqueeze(0)
-    with torch.no_grad():
-        prediction = torch.nn.functional.softmax(model(inp)[0], dim=0)
-        confidences = {labels[i]: float(prediction[i]) for i in range(1000)}    
-    return confidences
+def model(image, text):
+    if image is None:
+        return [(text, "请上传一张图片。")]
+    else:
+        response = pipe((text, image)).text
+        return [(text, response)]
 
-demo = gr.Interface(fn=predict, 
-                    inputs=gr.inputs.Image(type="pil"),
-                    outputs=gr.outputs.Label(num_top_classes=3),
-                    examples=[["cheetah.jpg"]],
-                   )
-             
-demo.launch()
+demo = gr.Interface(fn=model, inputs=[gr.Image(type="pil"), gr.Textbox()], outputs=gr.Chatbot())
+demo.launch()   
